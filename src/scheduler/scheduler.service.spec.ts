e265da4e1f7591fdb0e2ab4aebd5f710835a570e
sadraliev <a.sadraliev@gmail.com>
2024-08-31 00:34:55 -0400

    test(scheduler): Make convert date&time to UTC using IANA timezone

--- src/scheduler/scheduler.service.spec.ts
+++ src/scheduler/scheduler.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SchedulerService } from './scheduler.service';
+import { format } from 'date-fns';
+import { IANA } from 'src/lib';
 
 describe('SchedulerService', () => {
   let service: SchedulerService;
@@ -14,5 +16,34 @@ describe('SchedulerService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should convert time from America/New_York to UTC', () => {
+    const localDate = '2024-08-31T10:00:00'; // 10:00 AM in New York
+    const expectedDate = '2024-08-31T14:00:00.000Z'; // 10:00 AM in New York is 2:00 PM UTC
+    const date = new Date(localDate);
+    const timeZone: IANA = 'America/New_York';
+
+    const dateInUTC = service.convertToUTC(
+      format(date, "yyyy-MM-dd'T'HH:mm:ss"),
+      timeZone,
+    );
+
+    expect(dateInUTC.toISOString()).toBe(expectedDate);
+  });
+
+  it('should convert time from Asia/Bishkek to UTC', () => {
+    const localDate = '2024-08-31T10:00:00'; // 10:00 AM in Bishkek
+    const expectedDate = '2024-08-31T04:00:00.000Z'; // 10:00 AM in Bishkek is 4:00 AM UTC
+
+    const date = new Date(localDate);
+    const timeZone: IANA = 'Asia/Bishkek';
+
+    const dateInUTC = service.convertToUTC(
+      format(date, "yyyy-MM-dd'T'HH:mm:ss"),
+      timeZone,
+    );
+
+    expect(dateInUTC.toISOString()).toBe(expectedDate);
   });
 });
--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { fromZonedTime } from 'date-fns-tz';
+import { IANA } from 'src/lib';
 
 @Injectable()
-export class SchedulerService {}
+export class SchedulerService {
+  convertToUTC(iso8601: string, timeZone: IANA) {
+    return fromZonedTime(iso8601, timeZone);
+  }
+}
